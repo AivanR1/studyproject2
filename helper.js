e6d3de19a2817d822bcbd884dc66347f503ebbab
AivanR1 <ivanrom1721@gmail.com>
2023-06-24 13:01:36 +0300

    save22

---
+++ helper.js
@@ -0,0 +1,103 @@
+<atributes>
+  //! min
+  //? <input>, <meter>
+
+  //! max
+  //?<input>, <meter>, <progress></progress>
+
+  //! src 
+  //? <img> <link>
+
+  //! placeholder
+  //?
+
+
+  //! value
+  // ?<button>, <option>, <input>, <li>, <meter>, <progress>, <param> (en-US)
+
+ //! href
+ //? <a>, <area>, <base>, <link>
+
+ //! type
+ //?input object script source style menu
+
+ //! media
+ //? <a>, <area>, <link>, <source>, <style>
+
+ //! id
+ //? all
+
+ //! hidden
+ //? all
+
+ //! name
+ //? <button>, <form>, <fieldset>, <iframe> (en-US), <input>, <keygen>, <object> (en-US), <output>, <select>, <textarea> (en-US), <map>, <meta>, <param> (en-US)
+
+ //! form
+ //?<button>, <fieldset>, <input>, <keygen>, <label>, <meter>, <object> (en-US), <output>, <progress>, <select>, <textarea> (en-US)
+
+ //! download
+ //? <a>, <area>
+
+ //!disabled
+ //?<button>, <command>, <fieldset>, <input>, <keygen>, <optgroup>, <option>, <select>, <textarea> (en-US)
+
+ //!autofocus
+ //?<button>, <input>, <keygen>, <select>, <textarea> (en-US)
+ //!
+ //?
+</atributes>
+
+
+
+//tegs
+html
+body
+header
+title
+div
+section
+a
+link
+style
+nav
+img
+h12345
+p
+main
+footer
+// figure, figcaption 
+button
+input()
+text - текстове 
+password - поле  
+radio - перемикач.
+checkbox - прапорець.
+tel - 
+date 
+range 
+color 
+week 
+datetime
+
+// hidden - приховане поле.
+
+// button - кнопка.
+// submit - кнопка для відправки форми.
+// reset - кнопка для очищення форми.
+file 
+// image - кнопка із зображенням.)
+
+br
+script
+// picture
+span
+video
+audio
+
+select 
+option()
+
+// b, i
+ul, li
+hr- гориз.л
